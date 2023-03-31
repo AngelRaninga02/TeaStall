150a9a58eb5249dc650e3e7188ae7666cd56f4b8
AngelRaninga02 <112938609+AngelRaninga02@users.noreply.github.com>
2023-03-31 16:29:15 +0530

    Add files via upload

---
+++ Teastall/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Teastall project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Teastall.settings')
+
+application = get_asgi_application()
